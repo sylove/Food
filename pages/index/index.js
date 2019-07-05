3b5c6eb028cf0085c715395c9f6b304a809303f2
sy <1192052369@qq.com>
2019-07-05 17:53:55 +0800

    change

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,14 +9,10 @@ Page({
     indicatorDots:true,
     autoplay:true,
     interval:3000,
-    duration:1000
+    duration:1000,
+    goods:[]
   },
-  //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
+
   onLoad: function () {
     const that=this
     /**
@@ -30,39 +26,13 @@ Page({
         banners:res.data
       })
     })
-    // if (app.globalData.userInfo) {
-    //   this.setData({
-    //     userInfo: app.globalData.userInfo,
-    //     hasUserInfo: true
-    //   })
-    // } else if (this.data.canIUse){
-    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //   // 所以此处加入 callback 以防止这种情况
-    //   app.userInfoReadyCallback = res => {
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // } else {
-    //   // 在没有 open-type=getUserInfo 版本的兼容处理
-    //   wx.getUserInfo({
-    //     success: res => {
-    //       app.globalData.userInfo = res.userInfo
-    //       this.setData({
-    //         userInfo: res.userInfo,
-    //         hasUserInfo: true
-    //       })
-    //     }
-    //   })
-    // }
-  },
-  // getUserInfo: function(e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // }
+    WXAPI.goods({
+      recommendStatus: 1
+    }).then((res)=>{
+      console.log(res.data)
+      that.setData({
+        goods: res.data
+      })
+    })
+  }
 })
---
+++ pages/map/map.js
@@ -0,0 +1,30 @@
+// pages/map/map.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    markers:[{
+      iconPath:"",
+      id:0,
+      latitude: 29.4803082317,
+      longitude: 106.6301786900,
+      width: 30,
+      height: 30
+    }]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
